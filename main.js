e2b5c791bc671b589abf7c4ef8079f0637d8c472
Austin Flatt <austindflatt@gmail.com>
2021-09-29 11:17:24 -0400

    fixed challenge 15 and logged out arrays individually

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ console.log(students);
 // Pull the items at indices 4-6 (inclusive) from the following array using .splice (NOT .slice!), then console.log out each item from the ORIGINAL array. Note that .splice DOES change the original array.
 const dinosaurs = ['Velociraptor', 'T-Rex', 'Stegosaurus', 'Triceratops', 'Dimetrodon', 'Allosaur', 'Spinosaurus', 'Gigantosaur']
 const spliceDinosaurs = dinosaurs.splice(4, 3);
-console.log(spliceDinosaurs)
+console.log(spliceDinosaurs);
 
 
 // Challenge 13 - Complete
@@ -152,6 +152,9 @@ const primaries = ['red', 'yellow', 'blue'];
 const secondaries = ['orange', 'green', 'purple'];
 const combined = primaries.concat(secondaries);
 
-console.log(combined);
+for (let i = 0; i < combined.length; i++) {
+  const value = combined[i];
+  console.log(value);
+}
 console.log(primaries);
 console.log(secondaries);
